502551a0332f8f7bdd17acaba5b20074593662d2
iamnotss <91016729+iamnotss@users.noreply.github.com>
2024-12-09 11:54:23 +0800

    Delete 2023软件安全/学长资料合集 directory

--- 2023软件安全/学长资料合集/ScreenMonitorDemo/SocketScreenMonitorDemo/ScreenMonitor/src/client/GlobalVarClientRepo.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-public class GlobalVarClientRepo {
-	public static String repoFolderStr="ScreenCache";
-	public static String screenShotPrefixStr="/screenShot";
-	public static String screenShotTypeStr="jpg";
-	public static int MaxCaptureIndex=100;
-	public static int port=8000;
-}
